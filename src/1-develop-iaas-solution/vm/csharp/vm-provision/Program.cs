7144f724833c1270ba24974e773a078c7038b28b
Ahmad Luqman <ahmad-luqman@users.noreply.github.com>
2019-11-21 12:24:57 +0500

    feat(1-develop-iaas-solution): vm provisioning using csharp project initial structure

---
+++ src/1-develop-iaas-solution/vm/csharp/vm-provision/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.Azure.Management.ResourceManager.Fluent;
+
+namespace az203labs.vm_provision
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
